8bc8650eba98a2aeac593c0691bfc3562878095e
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2022-02-17 13:49:34 -0500

    navBar finished responsive

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/navBar/navBar.css"
 import chevron from "../../assets/images/chevron.png"
+import {IoIosSearch} from "react-icons/io"
 
 
 function NavBar() {
@@ -9,10 +10,10 @@ function NavBar() {
             <img className="navLogoContainer" src='https://i.natgeofe.com/n/1852daf6-1c8d-4428-8ee2-d9a82bd0401c/ng-border.png?w=33&h=48' alt= "small Nat Geo logo"/>
             <ul className="menuContainer">
                 <li><a href=".">LOGIN</a></li>
-                <li className="hideContent"><a href=".">(PONER IMAGEN DE LUPA)</a></li>
-                <li className="hideContent"><a href=".">Renew</a></li>
+                <li className="hideMobile"><a href="."><IoIosSearch/></a></li>
+                <li className="hideMobile"><a href=".">Renew</a></li>
                 <li className="subscribe"><a href=".">SUBSCRIBE</a></li>
-                <li className="hideContent"><a href=".">MENU</a></li>
+                <li className="hideMobile"><a href=".">MENU</a></li>
                 <li><a href="." ><img src={chevron} alt="chevron" className="chevron"/></a></li>
                 
             </ul>
