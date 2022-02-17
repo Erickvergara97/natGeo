3c8048680bbbd3032e7f30713e1e2939b7365d56
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2022-02-17 14:15:10 -0500

    navBar logos correction

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -7,7 +7,8 @@ import {IoIosSearch} from "react-icons/io"
 function NavBar() {
     return (
         <header className="navBarContainer">
-            <img className="navLogoContainer" src='https://i.natgeofe.com/n/1852daf6-1c8d-4428-8ee2-d9a82bd0401c/ng-border.png?w=33&h=48' alt= "small Nat Geo logo"/>
+            <img className="smallLogo" src='https://i.natgeofe.com/n/1852daf6-1c8d-4428-8ee2-d9a82bd0401c/ng-border.png?w=33&h=48' alt= "small Nat Geo logo"/>
+            <img className="bigLogo" src="https://i.natgeofe.com/n/e76f5368-6797-4794-b7f6-8d757c79ea5c/ng-logo-2fl.png?w=163.5&h=48"alt= "Nat Geo logo"/>
             <ul className="menuContainer">
                 <li><a href=".">LOGIN</a></li>
                 <li className="hideMobile"><a href="."><IoIosSearch/></a></li>
